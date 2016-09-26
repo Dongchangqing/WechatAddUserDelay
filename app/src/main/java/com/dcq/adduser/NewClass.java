edba077f66e32fb5c31f98b8383520517db7aa9f
Dongchangqing <285052101@qq.com>
2016-09-26 14:38:52 +0800

    new class  update

--- app/src/main/java/com/dcq/adduser/NewClass.java
+++ app/src/main/java/com/dcq/adduser/NewClass.java
@@ -1,7 +1,14 @@
 package com.dcq.adduser;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by Administrator on 2016/9/26 0026.
  */
-public class NewClass {
+public class NewClass extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
