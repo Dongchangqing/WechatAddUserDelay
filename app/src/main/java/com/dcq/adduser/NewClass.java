f7317bc5b810fc3d6d279d56df51048a2b85ff41
Dongchangqing <285052101@qq.com>
2016-09-26 14:43:02 +0800

    new class  update toast

--- app/src/main/java/com/dcq/adduser/NewClass.java
+++ app/src/main/java/com/dcq/adduser/NewClass.java
@@ -2,6 +2,7 @@ package com.dcq.adduser;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 /**
  * Created by Administrator on 2016/9/26 0026.
@@ -10,5 +11,6 @@ public class NewClass extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Toast.makeText(NewClass.this ,"这样提交一次试试",Toast.LENGTH_SHORT).show();
     }
 }
